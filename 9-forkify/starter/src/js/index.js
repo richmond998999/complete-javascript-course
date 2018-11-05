9edb9d0864fc42e517894b965a0721d061da68e5
Richmond Ivann Lim <rolim91@Richmonds-MacBook-Pro.local>
2018-11-05 00:25:48 -0800

    Save index.js

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,17 @@
 import Search from './models/Search';
 
+/** GLobal state of the app
+ *  - Search object
+ *  - Current recipt object
+ *  - Shopping list object
+ *  - Liked recipes
+ */
+const state = {}
+
+document.querySelector('.search').addEventListener('submit', e => {
+
+});
+
 const search = new Search('pizza');
 console.log(search);
-
-search.getResults();
+// search.getResults();
