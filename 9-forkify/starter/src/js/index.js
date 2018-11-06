f85b366dcd2da6c23ead5bf88f8756f12e1bcea9
Lim, Richmond - Contractor <ivlim@contractor.ea.com>
2018-11-05 17:01:05 -0800

    save

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView'; // searchView will be the object that have all the variables of searchView 
+import { elements } from './views/base';
 
 /** GLobal state of the app
  *  - Search object
@@ -8,10 +10,31 @@ import Search from './models/Search';
  */
 const state = {}
 
-document.querySelector('.search').addEventListener('submit', e => {
 
+// Add async and await since getResult returns a promise and is asynchronous
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if (query) {
+        // 2) New seach object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        // await state.search.getResults();
+        await state.search.getResultTimeout();
+
+        // 5) Render results on UI
+        // console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+    }
+}
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
 });
-
-const search = new Search('pizza');
-console.log(search);
-// search.getResults();
