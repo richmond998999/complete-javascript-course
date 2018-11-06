67236239693496f6507ee39b316f183fe8ccdfbb
Lim, Richmond - Contractor <ivlim@contractor.ea.com>
2018-11-05 17:29:33 -0800

    Add new implementation

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView'; // searchView will be the object that have all the variables of searchView 
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** GLobal state of the app
  *  - Search object
@@ -15,13 +15,15 @@ const state = {}
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput();
-    console.log(query);
 
     if (query) {
         // 2) New seach object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes
         // await state.search.getResults();
@@ -29,6 +31,7 @@ const controlSearch = async () => {
 
         // 5) Render results on UI
         // console.log(state.search.result);
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -2,6 +2,38 @@ import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+};
+
+// 'Pasta with tomato with spinach'
+/*
+acc: 0 / acc + curr.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + curr.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + curr.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + curr.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + curr.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, curr) => {    // reduce method has an accumulator built in
+            if (acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -10,15 +42,15 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
-}
+};
 
 export const renderResults = recipes => {
     recipes.forEach(renderRecipe);      // This will automatically pass each recipe to render recipe
-}
+};
